fa5a4e685ce1040a2ac2090a4f812a835aa1b36b
Fakhrul Anam <57746074+faamzero@users.noreply.github.com>
2020-11-04 08:30:48 +0700

    Add files via upload

---
+++ Praktikum 1/Perkenalan.php
@@ -0,0 +1,14 @@
+<?php 
+class Perkenalan extends CI_Controller{
+	public function index(){
+		 echo "<h1><center> Perkenalan </center></h1>";
+        echo "Nama saya Fakhrul Anam <br>
+              Saya tinggal di Tegal <br>
+              Saya suka bermain Tenis Meja
+              <hr> <hr>";
+        echo "<center> >>> .......cekk.......<<< </center>";
+
+	}
+}
+
+
